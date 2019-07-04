c4da5a623a5cc5b35c9c9f32e1e5b6874b70d0cd
yang0660 <yang0660@163.com>
2019-07-04 17:10:33 +0800

    重新生成mapper

--- src/main/java/com/myicellar/digitalmenu/dao/mapper/ProductMapper.java
+++ src/main/java/com/myicellar/digitalmenu/dao/mapper/ProductMapper.java
@@ -3,7 +3,15 @@ package com.myicellar.digitalmenu.dao.mapper;
 import com.myicellar.digitalmenu.dao.entity.Product;
 
 public interface ProductMapper {
+    int deleteByPrimaryKey(Long productId);
+
     int insert(Product record);
 
     int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Long productId);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
 }
--- src/main/java/com/myicellar/digitalmenu/dao/mapper/WineryMapper.java
+++ src/main/java/com/myicellar/digitalmenu/dao/mapper/WineryMapper.java
@@ -2,8 +2,18 @@ package com.myicellar.digitalmenu.dao.mapper;
 
 import com.myicellar.digitalmenu.dao.entity.Winery;
 
-public interface WineryMapper extends Mapper<Winery> {
+public interface WineryMapper {
+    int deleteByPrimaryKey(Long wineryId);
+
     int insert(Winery record);
 
     int insertSelective(Winery record);
+
+    Winery selectByPrimaryKey(Long wineryId);
+
+    int updateByPrimaryKeySelective(Winery record);
+
+    int updateByPrimaryKeyWithBLOBs(Winery record);
+
+    int updateByPrimaryKey(Winery record);
 }
