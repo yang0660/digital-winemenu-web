6919c3e1c88e1834400031edea1b3aa7467c5dd6
zhuhecheng <442119101@qq.com>
2019-07-24 14:47:40 +0800

    代码优化

---
+++ src/main/java/com/myicellar/digitalmenu/vo/request/ImgBatchDeleteReqVO.java
@@ -0,0 +1,16 @@
+package com.myicellar.digitalmenu.vo.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+* 图片删除参数
+*/
+@Data
+@ApiModel(value = "图片删除参数")
+public class ImgDeleteReqVO {
+
+    @ApiModelProperty(value = "图片ID")
+    private Long imgId;
+}
